d2925c903bea2f282ef302667d58e43b2483cca8
Rafael Chavez <rafaelchavezsolis@gmail.com>
2021-11-18 23:38:13 -0600

    Fix ImageCardComponent.jsx h-auto

--- src/components/cards/ImageCardComponent.jsx
+++ src/components/cards/ImageCardComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ImageCardComponent({ imageName, imageName2, children, hasbutton }) {
 	return (
 		<div className={!hasbutton && "h-full"}>
-			<div className="card bg-white shadow-xl mb-4 p-4 h-full">
+			<div className="card bg-white shadow-xl mb-4 p-4 h-auto">
 				<figure className={"max-h-full"}>
 					<img src={imageName} className="lg:hidden rounded-xl" alt="Algoritmo Interactivo" />
 					<img src={imageName2} className="hidden lg:block rounded-xl" alt="Algoritmo Interactivo" />
